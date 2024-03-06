211034084153a2e7ca7a8a7d412bf56ec19b0d0f
Durba Satpathi <70879852+durba-s@users.noreply.github.com>
2024-03-06 16:27:04 +0530

    Added navbar and initial page layouts

---
+++ blog/src/pages/about/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import StyledContainer from '../../components/styled-container';
+
+export default function About() {
+  return (
+    <StyledContainer>
+      <h1> About</h1>
+      <>
+      {"Work in progress"}
+      </>
+    </StyledContainer>
+  );
+}
