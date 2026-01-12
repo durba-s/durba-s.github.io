2abd7a9ef9daa5499d19dbd9a538779d92d6b16f
Durba Satpathi <durbasatpathi@Durbas-MacBook-Air.local>
2026-01-12 21:09:06 +0530

    updates

---
+++ blog/src/posts/p_rlintro.js
@@ -0,0 +1,11 @@
+export default {
+  slug: "rl-intro",
+  category: "Reinforcement Learning",
+  title: "Reinforcement Learning: An Introductory Overview",
+  date: "2025-12-26",
+  excerpt:
+    "A beginner-friendly introduction to Reinforcement Learning (RL)",
+  content: `
+## Introduction
+`
+};
